a0e915ef485207fde72d34f2aa967c1c5b30f4ad
Bret Jordan <jordan2175@gmail.com>
2016-09-14 12:25:07 -0600

    finished adding data to the example

--- examples/bundle/01-bundle.go
+++ examples/bundle/01-bundle.go
@@ -35,6 +35,26 @@ func main() {
 	infra.SetRegion("Europe")
 	infra.SetCountry("NL")
 
+	// Define some Observed Data for the Infrastructure
+	od1 := sm.NewObservedData()
+	od1.SetFirstObservedText("2016-09-01T00:00:01Z")
+	od1.SetLastObservedText("2016-09-07T00:00:01Z")
+	od1.SetNumberObserved(3)
+	od1.SetCybox("This will be a CybOX container object using the ipv4-addr object pointing to 5.79.68.0/24")
+
+	od2 := sm.NewObservedData()
+	od2.SetFirstObservedText("2016-09-07T00:00:01Z")
+	od2.SetLastObservedText("2016-09-14T00:00:01Z")
+	od2.SetNumberObserved(3)
+	od2.SetCybox("This will be a CybOX container object using the ipv4-addr object pointing to 5.79.52.0/24")
+
+	// Define some Observed Data for the sighting of the Infrastructure
+	od3 := sm.NewObservedData()
+	od3.SetFirstObservedText("2016-09-07T00:00:01Z")
+	od3.SetLastObservedText("2016-09-14T00:00:01Z")
+	od3.SetNumberObserved(1)
+	od3.SetCybox("This will be a CybOX container object using the ipv4-addr object pointing to 5.79.52.100")
+
 	// Define a family of malware
 	m1 := sm.NewMalware()
 	m1.SetName("Zeus")
@@ -88,6 +108,16 @@ func main() {
 	r5.SetRelationshipType("indicates")
 	r5.SetSourceTarget(i.GetId(), m2.GetId())
 
+	// Attach some Observed Data to an Infrastructure Object
+	r6 := sm.NewRelationship()
+	r6.SetRelationshipType("part-of")
+	r6.SetSourceTarget(od1.GetId(), infra.GetId())
+
+	// Attach some Observed Data to an Infrastructure Object
+	r7 := sm.NewRelationship()
+	r7.SetRelationshipType("part-of")
+	r7.SetSourceTarget(od2.GetId(), infra.GetId())
+
 	// Add a sighting for the malware
 	s1 := sm.NewSighting()
 	s1.SetFirstSeenText("2016-09-01T00:00:01Z")
@@ -101,6 +131,7 @@ func main() {
 	s2.SetLastSeenText("2016-09-01T10:30:00Z")
 	s2.SetCount(10)
 	s2.SetSightingOfRef(infra.GetId())
+	s2.AddObservedDataRef(od3.GetId())
 
 	var data []byte
 	data, _ = json.MarshalIndent(sm, "", "    ")
