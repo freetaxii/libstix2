e959f668d694801676d6c2fc4a51d4ee60911ea2
Bret Jordan <jordan2175@gmail.com>
2017-10-17 23:57:19 -0500

    finished adding support table creation

--- datastore/sqlite3/tables.go
+++ datastore/sqlite3/tables.go
@@ -22,16 +22,30 @@ func (ds *Sqlite3DatastoreType) CreateAllTables() {
 	ds.createTable("sdo_campaign", ds.campaignProperties())
 	ds.createTable("sdo_course_of_action", ds.courseOfActionProperties())
 	ds.createTable("sdo_identity", ds.identityProperties())
+	ds.createTable("identity_sectors", ds.identitySectorsProperties())
 	ds.createTable("sdo_indicator", ds.indicatorProperties())
 	ds.createTable("sdo_intrusion_set", ds.intrusionSetProperties())
+	ds.createTable("sdo_location", ds.locationProperties())
 	ds.createTable("sdo_malware", ds.malwareProperties())
+	ds.createTable("sdo_note", ds.noteProperties())
 	ds.createTable("sdo_observed_data", ds.observedDataProperties())
+	ds.createTable("sdo_opinion", ds.opinionProperties())
 	ds.createTable("sdo_report", ds.reportProperties())
-	ds.createTable("sdo_report_objects", ds.reportObjectsProperties())
 	ds.createTable("sdo_threat_actor", ds.threatActorProperties())
-	ds.createTable("sdo_threat_actor_goals", ds.threatActorGoalsProperties())
+	ds.createTable("threat_actor_roles", ds.threatActorRolesProperties())
 	ds.createTable("sdo_tool", ds.toolProperties())
 	ds.createTable("sdo_vulnerability", ds.vulnerabilityProperties())
+	ds.createTable("aliases", ds.commonAliasesProperties())
+	ds.createTable("authors", ds.commonAuthorsProperties())
+	ds.createTable("external_references", ds.commonExternalReferencesProperties())
+	ds.createTable("goals", ds.commonGoalsProperties())
+	ds.createTable("hashes", ds.commonHashesProperties())
+	ds.createTable("kill_chain_phases", ds.commonKillChainPhasesProperties())
+	ds.createTable("labels", ds.commonLabelsProperties())
+	ds.createTable("object_marking_refs", ds.commonObjectMarkingRefsProperties())
+	ds.createTable("object_refs", ds.commonObjectRefsProperties())
+	ds.createTable("secondary_motivations", ds.commonSecondaryMotivationsProperties())
+	ds.createTable("primary_motivations", ds.commonPersonalMotivationsProperties())
 }
 
 // ----------------------------------------------------------------------
@@ -48,13 +62,18 @@ func (ds *Sqlite3DatastoreType) createTable(name, properties string) {
 	}
 }
 
+// baseProperties - This method will return the base properties for all objects
+// row_id    = This is a database tracking number
+// object_id = This is a unique identifier for the STIX object based on its ID + created and modified timestamps
 func (ds *Sqlite3DatastoreType) baseProperties() string {
 	return `
 	"row_id" INTEGER PRIMARY KEY,
  	"object_id" TEXT NOT NULL,`
 }
 
-// commonProperties - This method will return the the common properties
+// baseObjectProperties - This method will return the the common properties
+// version    = STIX specification version
+// date_added = TAXII, the date the object was added to the TAXII server
 func (ds *Sqlite3DatastoreType) baseObjectProperties() string {
 	return ds.baseProperties() + `
  	"version" TEXT NOT NULL,
@@ -101,15 +120,22 @@ func (ds *Sqlite3DatastoreType) identityProperties() string {
 	return ds.baseProperties() + `
 	"name" TEXT NOT NULL,
 	"description" TEXT,
-	"identity_class" INTEGER NOT NULL,
+	"identity_class" TEXT NOT NULL,
 	"contact_information" TEXT
+	`
+}
+
+// identitySectorsProperties  - This method will return the properties for identity sectors
+func (ds *Sqlite3DatastoreType) identitySectorsProperties() string {
+	return ds.baseProperties() + `
+	"sectors" TEXT NOT NULL
 	`
 }
 
 // indicatorProperties  - This method will return the properties for indicator SDOs
 func (ds *Sqlite3DatastoreType) indicatorProperties() string {
 	return ds.baseProperties() + `
-	"name" TEXT NOT NULL,
+	"name" TEXT,
 	"description" TEXT,
 	"pattern" TEXT NOT NULL,
 	"valid_from" TEXT NOT NULL,
@@ -124,8 +150,24 @@ func (ds *Sqlite3DatastoreType) intrusionSetProperties() string {
 	"description" TEXT,
 	"first_seen" TEXT,
 	"last_seen" TEXT,
-	"resource_level" INTEGER,
+	"resource_level" TEXT,
 	"primary_motivation" TEXT
+	`
+}
+
+// locationProperties - This method will return the properties for location SDOs
+func (ds *Sqlite3DatastoreType) locationProperties() string {
+	return ds.baseProperties() + `
+	"description" TEXT,
+	"latitude" TEXT,
+	"longitude" TEXT,
+	"precision" TEXT,
+	"region" TEXT,
+	"country" TEXT,
+	"administrative_area" TEXT,
+	"city" TEXT,
+	"street_address" TEXT,
+	"postal_code" TEXT
 	`
 }
 
@@ -134,6 +176,14 @@ func (ds *Sqlite3DatastoreType) malwareProperties() string {
 	return ds.baseProperties() + `
 	"name" TEXT NOT NULL,
 	"description" TEXT
+	`
+}
+
+// noteProperties  - This method will return the properties for note SDOs
+func (ds *Sqlite3DatastoreType) noteProperties() string {
+	return ds.baseProperties() + `
+	"summary" TEXT,
+	"description" TEXT NOT NULL
 	`
 }
 
@@ -147,6 +197,14 @@ func (ds *Sqlite3DatastoreType) observedDataProperties() string {
 	`
 }
 
+// opinionProperties - This method will return the properties for opinion SDOs
+func (ds *Sqlite3DatastoreType) opinionProperties() string {
+	return ds.baseProperties() + `
+	"description" TEXT,
+	"opinion" TEXT
+	`
+}
+
 // reportProperties  - This method will return the properties for report SDOs
 func (ds *Sqlite3DatastoreType) reportProperties() string {
 	return ds.baseProperties() + `
@@ -156,28 +214,21 @@ func (ds *Sqlite3DatastoreType) reportProperties() string {
 	`
 }
 
-// reportObjectsProperties  - This method will return the properties for report objects
-func (ds *Sqlite3DatastoreType) reportObjectsProperties() string {
-	return ds.baseProperties() + `
-	"object_refs" TEXT NOT NULL
-	`
-}
-
 // threatActorProperties  - This method will return the properties for threat actor SDOs
 func (ds *Sqlite3DatastoreType) threatActorProperties() string {
 	return ds.baseProperties() + `
 	"name" TEXT NOT NULL,
 	"description" TEXT,
-	"sophistication" INTEGER,
-	"resource_level" INTEGER,
+	"sophistication" TEXT,
+	"resource_level" TEXT,
 	"primary_motivation" TEXT
 	`
 }
 
-// threatActorGoalsProperties  - This method will return the properties for threat actor goals
-func (ds *Sqlite3DatastoreType) threatActorGoalsProperties() string {
+// threatActorRolesProperties  - This method will return the properties for threat actor roles
+func (ds *Sqlite3DatastoreType) threatActorRolesProperties() string {
 	return ds.baseProperties() + `
-	"goals" TEXT NOT NULL
+	"roles" TEXT NOT NULL
 	`
 }
 
@@ -205,6 +256,13 @@ func (ds *Sqlite3DatastoreType) commonAliasesProperties() string {
 	`
 }
 
+// commonAuthorsProperties - This method will return the properties for common authors
+func (ds *Sqlite3DatastoreType) commonAuthorsProperties() string {
+	return ds.baseProperties() + `
+	"authors" TEXT NOT NULL
+	`
+}
+
 // commonExternalReferencesProperties - This method will return the properties for external references
 func (ds *Sqlite3DatastoreType) commonExternalReferencesProperties() string {
 	return ds.baseProperties() + `
@@ -212,6 +270,13 @@ func (ds *Sqlite3DatastoreType) commonExternalReferencesProperties() string {
 	"description" TEXT,
 	"url" TEXT,
 	"external_id" TEXT
+	`
+}
+
+// commonGoalsProperties  - This method will return the properties for goals
+func (ds *Sqlite3DatastoreType) commonGoalsProperties() string {
+	return ds.baseProperties() + `
+	"goals" TEXT NOT NULL
 	`
 }
 
@@ -234,14 +299,7 @@ func (ds *Sqlite3DatastoreType) commonKillChainPhasesProperties() string {
 // commonLabelsProperties - This method will return the properties for labels
 func (ds *Sqlite3DatastoreType) commonLabelsProperties() string {
 	return ds.baseProperties() + `
-	"labels" TEXT
-	`
-}
-
-// commonSecondaryMotivationsProperties - This method will return the properties for secondary motivations
-func (ds *Sqlite3DatastoreType) commonSecondaryMotivationsProperties() string {
-	return ds.baseProperties() + `
-	"secondary_motivations" TEXT
+	"labels" TEXT NOT NULL
 	`
 }
 
@@ -249,5 +307,26 @@ func (ds *Sqlite3DatastoreType) commonSecondaryMotivationsProperties() string {
 func (ds *Sqlite3DatastoreType) commonObjectMarkingRefsProperties() string {
 	return ds.baseProperties() + `
 	"object_marking_refs" TEXT NOT NULL
+	`
+}
+
+// commonObjectRefsProperties - This method will return the properties for object refs
+func (ds *Sqlite3DatastoreType) commonObjectRefsProperties() string {
+	return ds.baseProperties() + `
+	"object_refs" TEXT NOT NULL
+	`
+}
+
+// commonPersonalMotivationsProperties - This method will return the properties for personal motivations
+func (ds *Sqlite3DatastoreType) commonPersonalMotivationsProperties() string {
+	return ds.baseProperties() + `
+	"personal_motivations" TEXT NOT NULL
+	`
+}
+
+// commonSecondaryMotivationsProperties - This method will return the properties for secondary motivations
+func (ds *Sqlite3DatastoreType) commonSecondaryMotivationsProperties() string {
+	return ds.baseProperties() + `
+	"secondary_motivations" TEXT NOT NULL
 	`
 }
