24b4d9f895565ac3d366e22a9a43e86a487970c7
Bret Jordan <jordan2175@gmail.com>
2018-04-27 22:35:06 -0600

    cleanup

--- defs/defs.go
+++ defs/defs.go
@@ -9,10 +9,14 @@ package defs
 // TIME_RFC_3339 is the general purpose timestamp.
 // TIME_RFC_3339_MICRO is the timestamp for the created and modified properties.
 const (
-	STIX_MEDIA_TYPE  = "application/vnd.oasis.stix+json"
-	STIX_VERSION     = "version=2.1"
-	TAXII_MEDIA_TYPE = "application/vnd.oasis.taxii+json"
-	TAXII_VERSION    = "version=2.0"
+	STIX_MEDIA_TYPE    = "application/stix+json"
+	STIX_VERSION       = "2.1"
+	TAXII_MEDIA_TYPE   = "application/taxii+json"
+	TAXII_VERSION      = "2.1"
+	CONTENT_TYPE_STIX  = STIX_MEDIA_TYPE + "; version=" + STIX_VERSION + "; charset=utf-8"
+	CONTENT_TYPE_TAXII = TAXII_MEDIA_TYPE + "; version=" + TAXII_VERSION + "; charset=utf-8"
+	CONTENT_TYPE_JSON  = "application/json; charset=utf-8"
+	CONTENT_TYPE_HTML  = "text/html; charset=utf-8"
 
 	TIME_RFC_3339       = "2006-01-02T15:04:05Z07:00"
 	TIME_RFC_3339_MILLI = "2006-01-02T15:04:05.999Z07:00"
