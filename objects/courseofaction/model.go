0c678fba43532e4df2e9cce6557745e7dd29a4e0
Bret Jordan <jordan2175@gmail.com>
2020-01-02 15:59:39 -0700

    started updating to 2.1

---
+++ objects/courseofaction/model.go
@@ -0,0 +1,39 @@
+// Copyright 2015-2020 Bret Jordan, All rights reserved.
+//
+// Use of this source code is governed by an Apache 2.0 license that can be
+// found in the LICENSE file in the root of the source tree.
+
+package courseofaction
+
+import (
+	"github.com/freetaxii/libstix2/objects"
+	"github.com/freetaxii/libstix2/objects/properties"
+)
+
+// ----------------------------------------------------------------------
+// Define Object Type
+// ----------------------------------------------------------------------
+
+/* CourseOfAction - This type implements the STIX 2 Course Of Action SDO and
+defines all of the properties and methods needed to create and work with this
+object. All of the methods not defined local to this type are inherited from the
+individual properties. */
+type CourseOfAction struct {
+	objects.CommonObjectProperties
+	properties.NameProperty
+	properties.DescriptionProperty
+}
+
+// TODO Finish fleshing out this model to 2.1
+
+// ----------------------------------------------------------------------
+// Initialization Functions
+// ----------------------------------------------------------------------
+
+/* New - This function will create a new STIX Course of Action object and return
+it as a pointer. */
+func New() *CourseOfAction {
+	var obj CourseOfAction
+	obj.InitSDO("course-of-action")
+	return &obj
+}
