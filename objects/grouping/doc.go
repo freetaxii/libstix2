6d4d17ff2988f8220c3de0b280257ca45c167433
Bret Jordan <jordan2175@gmail.com>
2020-01-02 16:20:50 -0700

    started updating to 2.1

---
+++ objects/grouping/doc.go
@@ -0,0 +1,27 @@
+// Copyright 2015-2020 Bret Jordan, All rights reserved.
+//
+// Use of this source code is governed by an Apache 2.0 license that can be
+// found in the LICENSE file in the root of the source tree.
+
+/*
+Package grouping implements the STIX 2.1 Grouping object.
+
+The following information comes directly from the STIX 2.1 specification.
+
+A Grouping object explicitly asserts that the referenced STIX Objects have a
+shared context, unlike a STIX Bundle (which explicitly conveys no context). A
+Grouping object should not be confused with an intelligence product, which
+should be conveyed via a STIX Report.
+
+A STIX Grouping object might represent a set of data that, in time, given
+sufficient analysis, would mature to convey an incident or threat report as a
+STIX Report object. For example, a Grouping could be used to characterize an
+ongoing investigation into a security event or incident. A Grouping object could
+also be used to assert that the referenced STIX Objects are related to an
+ongoing analysis process, such as when a threat analyst is collaborating with
+others in their trust community to examine a series of Campaigns and Indicators.
+The Grouping SDO contains a list of references to SDOs, SCOs, and SROs, along
+with an explicit statement of the context shared by the content, a textual
+description, and the name of the grouping.
+*/
+package grouping
---
+++ objects/grouping/valid.go
@@ -0,0 +1,50 @@
+// Copyright 2015-2020 Bret Jordan, All rights reserved.
+//
+// Use of this source code is governed by an Apache 2.0 license that can be
+// found in the LICENSE file in the root of the source tree.
+
+package grouping
+
+import "fmt"
+
+// ----------------------------------------------------------------------
+// Public Methods
+// ----------------------------------------------------------------------
+
+/* Valid - This method will verify and test all of the properties on an object
+to make sure they are valid per the specification. It will return a boolean, an
+integer that tracks the number of problems found, and a slice of strings that
+contain the detailed results, whether good or bad. */
+func (o *Grouping) Valid() (bool, int, []string) {
+	problemsFound := 0
+	resultDetails := make([]string, 0)
+
+	// Check common base properties first
+	_, pBase, dBase := o.CommonObjectProperties.ValidSDO()
+	problemsFound += pBase
+	resultDetails = append(resultDetails, dBase...)
+
+	if o.Context == "" {
+		problemsFound++
+		str := fmt.Sprintf("-- The context property is required but missing")
+		resultDetails = append(resultDetails, str)
+	} else {
+		str := fmt.Sprintf("++ The context property is required and is present")
+		resultDetails = append(resultDetails, str)
+	}
+
+	if len(o.ObjectRefs) == 0 {
+		problemsFound++
+		str := fmt.Sprintf("-- The object_refs property is required but missing")
+		resultDetails = append(resultDetails, str)
+	} else {
+		str := fmt.Sprintf("++ The object_refs property is required and is present")
+		resultDetails = append(resultDetails, str)
+	}
+
+	if problemsFound > 0 {
+		return false, problemsFound, resultDetails
+	}
+
+	return true, 0, resultDetails
+}
