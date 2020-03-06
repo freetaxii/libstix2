e810c98d34673e2e93e192eb16935d596365d4f5
andrewdrummond <waddrummond@gmail.com>
2020-03-05 20:15:59 -0700

    Update collections.go
    Fixed package paths

--- objects/taxii/collections/collections.go
+++ objects/taxii/collections/collections.go
@@ -6,10 +6,10 @@
 package collections
 
 import (
-	"github.com/freetaxii/libstix2/objects/envelope"
-	"github.com/freetaxii/libstix2/objects/manifest"
+	"github.com/freetaxii/libstix2/objects/taxii/envelope"
+	"github.com/freetaxii/libstix2/objects/taxii/manifest"
 	"github.com/freetaxii/libstix2/objects/properties"
-	"github.com/freetaxii/libstix2/objects/versions"
+	"github.com/freetaxii/libstix2/objects/taxii/versions"
 )
 
 // ----------------------------------------------------------------------
