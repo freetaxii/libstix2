077f284a0a5c3af4ebe125365af30509870e8f98
Konstantin Papesh <konstantin.papesh@fh-hagenberg.at>
2022-07-26 16:20:12 +0200

    Fixed AdditionalHeaderFields mapping

--- objects/sco/emailmessage/model.go
+++ objects/sco/emailmessage/model.go
@@ -21,21 +21,21 @@ individual properties.
 */
 type EmailMessage struct {
 	objects.CommonObjectProperties
-	IsMultipart            bool              `json:"is_multipart,omitempty" bson:"is_multipart,omitempty"`
-	Date                   string            `json:"date,omitempty" bson:"date,omitempty"`
-	ContentType            string            `json:"content_type,omitempty" bson:"content_type,omitempty"`
-	FromRef                string            `json:"from_ref,omitempty" bson:"from_ref,omitempty"`
-	SenderRef              string            `json:"sender_ref,omitempty" bson:"sender_ref,omitempty"`
-	ToRefs                 []string          `json:"to_refs,omitempty" bson:"to_refs,omitempty"`
-	CcRefs                 []string          `json:"cc_refs,omitempty" bson:"cc_refs,omitempty"`
-	BccRefs                []string          `json:"bcc_refs,omitempty" bson:"bcc_refs,omitempty"`
-	MessageId              string            `json:"message_id,omitempty" bson:"message_id,omitempty"`
-	Subject                string            `json:"subject,omitempty" bson:"subject,omitempty"`
-	ReceivedLines          []string          `json:"received_lines,omitempty" bson:"received_lines,omitempty"`
-	AdditionalHeaderFields map[string]string `json:"additional_header_fields,omitempty" bson:"additional_header_fields,omitempty"`
-	Body                   string            `json:"body,omitempty" bson:"body,omitempty"`
-	BodyMultipart          []string          `json:"body_multipart,omitempty" bson:"body_multipart,omitempty"`
-	RawEmailRef            string            `json:"raw_email_ref,omitempty" bson:"raw_email_ref,omitempty"`
+	IsMultipart            bool           `json:"is_multipart,omitempty" bson:"is_multipart,omitempty"`
+	Date                   string         `json:"date,omitempty" bson:"date,omitempty"`
+	ContentType            string         `json:"content_type,omitempty" bson:"content_type,omitempty"`
+	FromRef                string         `json:"from_ref,omitempty" bson:"from_ref,omitempty"`
+	SenderRef              string         `json:"sender_ref,omitempty" bson:"sender_ref,omitempty"`
+	ToRefs                 []string       `json:"to_refs,omitempty" bson:"to_refs,omitempty"`
+	CcRefs                 []string       `json:"cc_refs,omitempty" bson:"cc_refs,omitempty"`
+	BccRefs                []string       `json:"bcc_refs,omitempty" bson:"bcc_refs,omitempty"`
+	MessageId              string         `json:"message_id,omitempty" bson:"message_id,omitempty"`
+	Subject                string         `json:"subject,omitempty" bson:"subject,omitempty"`
+	ReceivedLines          []string       `json:"received_lines,omitempty" bson:"received_lines,omitempty"`
+	AdditionalHeaderFields map[string]any `json:"additional_header_fields,omitempty" bson:"additional_header_fields,omitempty"`
+	Body                   string         `json:"body,omitempty" bson:"body,omitempty"`
+	BodyMultipart          []string       `json:"body_multipart,omitempty" bson:"body_multipart,omitempty"`
+	RawEmailRef            string         `json:"raw_email_ref,omitempty" bson:"raw_email_ref,omitempty"`
 }
 
 /*
